0c6586c7370d2911f29d91caffcf1b275cda93dd
Mike Stromme <mstromme@suburbanenterprises.com>
2022-03-03 09:23:47 -0600

    new file

---
+++ network_scanner/network_scanner_new.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--t", dest="target", help="Target MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please provide a target, use --help for more info.")
+    return options
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    # arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    # arp_request_broadcast.show()
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    print("IP\t\t\tMAC Address\n------------------------------------------")
+
+    for element in answered_list:
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
