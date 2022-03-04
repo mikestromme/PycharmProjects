8d602f6f464ed7778c275db82393bffd577bba4e
mikestromme <mikestromme@gmail.com>
2022-03-04 19:32:13 +0000

    updated file

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
 
-def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Get the MAC address of the target ip")
+    (options) = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify a target, use --help for more info.")
+    return options
+
+
+def scan(target):
+    arp_request = scapy.ARP(pdst=target)
     # arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     # broadcast.show()
@@ -11,12 +21,25 @@ def scan(ip):
     # arp_request_broadcast.show()
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("IP\t\t\tMAC Address\n------------------------------------------")
+    clients_list = []
 
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"] )
 
 
 
-scan("192.168.198.1/24")
+
+# get the command line options
+options = get_arguments()
+# call the scan function and save result as variable
+scan_result = scan(options.target)
+#print results
+print_result(scan_result)
 
